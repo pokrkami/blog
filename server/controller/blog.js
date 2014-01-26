fa617e26e781502b4e10dc015fb1981a753a8ea8
Takahiro Murakami <kemutaka@gmail.com>
2014-01-26 15:58:01 +0900

    ブログコントローラー

---
+++ server/controller/blog.js
@@ -0,0 +1,43 @@
+
+/*
+ * GET home page.
+ */
+
+//DB設定
+var mysql = require('../config/db');
+var connection = mysql.dbConect.blog();
+
+exports.blogController = {
+
+    'top' : function(req, res){
+
+            var queryStr = 'select * from blog limit=10';
+            connection.query(queryStr, [ new Date() ], function(err, rows) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log('db中身' + debug(rows));
+                    res.render('index', { articles: rows });
+                }
+            });
+
+
+
+
+    },
+    'postBlog' : function(req , res){
+        var body = req.body;
+        insertBlogQuery = 'insert into blog (title, content) values("' + body.title + '","' + body.content + '")';
+        connection.query(insertBlogQuery , function(err, rows){
+            if(err) {
+                console.log(err);
+            } else {
+                res.redirect('/');
+            }
+        });
+
+
+    }
+
+
+};
