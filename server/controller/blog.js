0e7cfb5f665feb65a9362f3db47ffbba80f12f8b
Takahiro Murakami <kemutaka@gmail.com>
2014-02-03 00:07:22 +0900

    データ制御をmodelに移植

--- server/controller/blog.js
+++ server/controller/blog.js
@@ -3,45 +3,27 @@
  * GET home page.
  */
 
-//DB設定
 var mysql = require('../config/db');
 var connection = mysql.dbConect.blog();
+var article = require ('../model/article');
+
 
 exports.blogController = {
 
     'top' : function(req, res){
 
-            var queryStr = 'select * from blog';
-            connection.query(queryStr, [ new Date() ], function(err, rows) {
-                if(err) {
-                    console.log(err);
-                } else {
-                    console.log('db中身' + debug(rows));
-                    res.render('index', { articles: rows });
-                }
-            });
 
-
+        article.getAllArticle(req , res , function(data){
+//            console.log(debug(data));
+            res.render('index', { articles: data });
+        });
 
 
     },
     'postBlog' : function(req , res){
-        var body = req.body;
-        var DateObj = new Date();
-        var datetimeAry = [DateObj.getFullYear() , DateObj.getMonth() + 1, DateObj.getDate() , DateObj.getHours() , DateObj.getMinutes() , DateObj.getSeconds()];
-        var dateTime = datetimeAry.join('-');
-        console.log(debug(dateTime));
-        insertBlogQuery = 'insert into blog (title, content , create_time ,update_time) values("' + body.title + '","' + body.content + '")';
-        connection.query(insertBlogQuery , function(err, rows){
-            if(err) {
-                console.log(err);
-            } else {
-                res.redirect('/');
-            }
+
+        article.post(req , res ,function(){
+            res.redirect('/');
         });
-
-
     }
-
-
 };
