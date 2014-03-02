41c5edef73a6139958b87ef749d30d74965d917f
Takahiro Murakami <kemutaka@gmail.com>
2014-03-02 20:12:48 +0900

    認証、マイページの処理追加

--- server/controller/blog.js
+++ server/controller/blog.js
@@ -6,6 +6,7 @@
 var mysql = require('../config/db');
 var connection = mysql.dbConect.blog();
 var article = require ('../model/article');
+var auth = require ('../model/auth');
 
 
 exports.blogController = {
@@ -14,7 +15,8 @@ exports.blogController = {
 
 
         article.getAllArticle(req , res , function(data){
-//            console.log(debug(data));
+//            var msg = req.sessionID + 'on' + process.pid;
+
             res.render('index', { articles: data });
         });
 
@@ -25,5 +27,44 @@ exports.blogController = {
         article.post(req , res ,function(){
             res.redirect('/');
         });
+    },
+
+    'login'  : function(req , res){
+        if(req.session.flg){
+            res.redirect('/mypage');
+        }else{
+            res.render('login');
+        }
+
+
+    },
+
+    'doLogin'  : function(req , res){
+        auth.login(req , res ,function(){
+            res.redirect('/mypage');
+        });
+
+
+
+    },
+
+
+    'myPage'  : function(req , res){
+
+        if(req.session.flg){
+            res.render('mypage');
+        } else {
+            res.redirect('/login');
+        }
+
+
+
+    },
+
+    'about' : function(req , res){
+        res.render('about');
     }
+
+
+
 };
