625a1427cdd8cc1cf7d4647f20c3d5b2e8a2c604
Takahiro Murakami <kemutaka@gmail.com>
2014-06-22 16:46:09 +0900

    googleアナリティクス追加

---
+++ public/js/common.js
@@ -0,0 +1,17 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: taka
+ * Date: 2014/06/22
+ * Time: 16:32
+ * To change this template use File | Settings | File Templates.
+ */
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-17060342-3', 'kemutaka.com');
+ga('send', 'pageview');
+
+</script>
